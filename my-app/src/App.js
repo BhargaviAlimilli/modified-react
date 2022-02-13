af07563a465a2382d0d2ebe91b70cd4c125194fb
BhargaviAlimilli <bhargavi.alimilli@gmail.com>
2022-02-13 19:08:17 +0530

    Statest and events together

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 // import States from './StateInAction'
-import Events from './SimpleEvents'
+// import Events from './SimpleEvents'
+import StatesEvents from './StatesAndEvents';
 
 function App() {
   return (
     <div className="App">
       <h1> sanity check</h1>
       {/* <States name="Harry potter" /> */}
-      <Events />
+      {/* <Events /> */}
+      <StatesEvents />
     </div>
   );
 }
