089c4daab612855dd4142f1623e41a02f9520c5b
BhargaviAlimilli <bhargavi.alimilli@gmail.com>
2022-02-14 01:05:15 +0530

    state practice

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,8 @@
 import './App.css';
 // import States from './StateInAction'
 // import Events from './SimpleEvents'
-import StatesEvents from './StatesAndEvents';
+// import StatesEvents from './StatesAndEvents';
+import StatePractice from './StatePractice';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <h1> sanity check</h1>
       {/* <States name="Harry potter" /> */}
       {/* <Events /> */}
-      <StatesEvents />
+      {/* <StatesEvents /> */}
+      <StatePractice />
     </div>
   );
 }
