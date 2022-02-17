c9c9fcdcbab38e4208677df54da1876f4504376d
BhargaviAlimilli <bhargavi.alimilli@gmail.com>
2022-02-17 23:23:12 +0530

    Adding weather icon, adding input for city name and dynamic rendering of weather

--- section3of1/src/App.js
+++ section3of1/src/App.js
@@ -12,46 +12,55 @@ class App extends React.Component {
       cityName: "",
       weather: "",
       high: "",
-      low: ""
+      low: "",
+      icon:""
     }
     console.log("contructor running")
   
   }
 
   componentDidMount(){
-    const url= 'https://api.openweathermap.org/data/2.5/weather?lat=16.703133&lon=81.104015&appid=166b3b85b36db1e454f6fbc2ebd2de09'
-
-    axios.get(url).then((resp)=>{
-    console.log(resp.data)
-    console.log("did mount is running")
-    this.setState({
-      temp: resp.data.main.temp,
-      high: resp.data.main.temp_max,
-      low: resp.data.main.temp_min,
-      weather: resp.data.weather[0].description,
-      // icon: resp.data.weather[0].icon,
-      cityName: resp.data.name  
-    })
-    })
-
     var elems = document.querySelectorAll('.modal');
-    var instances = window.M.Modal.init(elems);
+    var instances = window.M.Modal.init(elems)
 
+  }
 
+  searchCity = (e)=>{
+    e.preventDefault();
+    const city = document.getElementById('city').value;
+    this.getCityWeather(city);
+  }
+
+  getCityWeather = (city)=>{
+    const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=166b3b85b36db1e454f6fbc2ebd2de09`
+    axios.get(url).then((resp)=>{
+      this.setState({
+        temp: resp.data.main.temp,
+        high: resp.data.main.temp_max,
+        low: resp.data.main.temp_min,
+        weather: resp.data.weather[0].description,
+        icon: resp.data.weather[0].icon,
+        cityName: resp.data.name
+      })
+    })    
   }
 
   render(){
 
     console.log("render is running")
+    const iconUrl=`http://openweathermap.org/img/wn/${this.state.icon}@2x.png`
 
   return (
     <div className="App">
       <a className="waves-effect waves-light btn modal-trigger" href="#modal1">Check Weather</a>
+      <form onSubmit={this.searchCity}>
+      <input type='text' placeholder='Enter the city name' id='city'></input>
+      </form>
 
     <div id="modal1" className="modal">
       <div className="modal-content">
         <h2>{this.state.cityName}</h2>
-        <h5>{this.state.weather}</h5>
+        <h5>{this.state.weather}</h5> <img src={iconUrl} />
         <p>max temp-{this.state.high}, min temp-{this.state.low}</p>
       </div>
   <div className="modal-footer">
