9ebfae9b96d63d74279c8a635ecf58db3feaae89
BhargaviAlimilli <bhargavi.alimilli@gmail.com>
2022-02-17 16:55:25 +0530

    weather api with class components

--- section3of1/src/App.js
+++ section3of1/src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React from 'react'
 import './App.css';
+import axios from 'axios'
 
-function App() {
+class App extends React.Component {
+
+  constructor(){
+    super()
+  
+  const url= 'https://api.openweathermap.org/data/2.5/weather?lat=16.703133&lon=81.104015&appid=166b3b85b36db1e454f6fbc2ebd2de09'
+
+  axios.get(url).then((resp)=>{
+    console.log(resp.data)
+  })
+  }
+
+  render(){
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Sanity check</h1>
+      
     </div>
   );
+ }
 }
 
 export default App;
